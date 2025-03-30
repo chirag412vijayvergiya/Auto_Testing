67c0d583b181f5d99f8d34273d5fd90ac01b7d75
chiragvijayvergiya <chirag412vv@gmail.com>
2025-03-30 23:59:05 +0530

    dffs

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoute');
 const app = express();
 app.use(express.json());
-app.use('/api', userRoutes);
+app.use('/api', userRoutes );
 module.exports = app ;
